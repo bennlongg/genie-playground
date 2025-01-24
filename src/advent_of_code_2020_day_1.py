ee0e06d8daf2bb3bef6bad8d1224def76514fa36
Ben Long <ben.long@cosine.sh>
2025-01-24 09:21:15 +0000

    feat: add solution for Advent of Code 2020 Day 1
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ src/advent_of_code_2020_day_1.py
@@ -0,0 +1,7 @@
+with open('input.txt', 'r') as file:
+    data = {int(number) for number in file}
+
+print({entry * (2020-entry) for entry in data if (2020-entry) in data})
+print({entry1 * entry2 * (2020 - entry1 - entry2)
+       for entry1 in data for entry2 in data
+       if (2020 - entry1 - entry2) in data})
