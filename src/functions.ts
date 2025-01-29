42ff1764230a486ffaa87466211cdcb203609ca2
Ben Long <ben.long@cosine.sh>
2025-01-29 09:45:57 +0000

    feat: add new joke to the getRandomJoke function
    Co-authored-by: Genie <genie@cosine.sh>

--- src/functions.ts
+++ src/functions.ts
@@ -11,7 +11,8 @@ export function getRandomJoke(): string {
     "What do you call fake spaghetti? An impasta!",
     "Why did the bicycle fall over? Because it was two-tired!",
     "Two hunters are out in the woods when one of them collapses. He doesn't seem to be breathing and his eyes are glazed. The other guy whips out his phone and calls emergency services. He gasps, 'My friend is dead! What can I do?' The operator says, 'Calm down. I can help. First, let's make sure he's dead.' There is a silence; then a gunshot is heard. Back on the phone, the guy says, 'OK, now what?'",
-    "Why did Adele cross the road? To say hello from the other side."
+    "Why did Adele cross the road? To say hello from the other side.",
+    "What do you call a fish wearing a bowtie? Sofishticated."
   ];
 
   const randomIndex = Math.floor(Math.random() * jokes.length);
