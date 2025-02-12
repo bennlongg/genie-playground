e83047e6a59c62acd0f688daf783aad828c089eb
Ben Long <ben.long@cosine.sh>
2025-02-12 23:10:52 +0000

    feat: add a new console log for Fibonacci numbers
    Co-authored-by: Genie <genie@cosine.sh>

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { fibonacci } from "./functions";
 
 console.log("Hello Genie!");
 console.log("10th Fibonacci Number:", fibonacci(10));
+console.log("Fibonacci Numbers are like secrets")
 console.log("GOODBYE")
