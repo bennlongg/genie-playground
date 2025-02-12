e193ea042fcef4b74785bf465dab11e39bed6745
Ben Long <ben.long@cosine.sh>
2025-02-12 23:06:04 +0000

    feat: add new message about invented word in index.ts
    Co-authored-by: Genie <genie@cosine.sh>

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import { fibonacci } from "./functions";
 console.log("Hello Genie!");
 console.log("10th Fibonacci Number:", fibonacci(10));
 console.log("GOODBYE")
+console.log("I invented a new word! Plagiarism.")
