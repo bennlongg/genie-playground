45b4d62caeaf7756273c90a825715cf4c460ab46
Ben Long <ben.long@cosine.sh>
2025-02-13 16:52:44 +0000

    feat: add documentation for fibonacci function
    Co-authored-by: Genie <genie@cosine.sh>

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,8 @@
+/**
+ * Calculates nth fibonacci number.
+ * @param {number} n Nth postion of fibonacci to calculate. N should not be negative number
+ * @returns {number} Nth fibonacci number to calculate
+ */
 export function fibonacci(n: number): number {
     if (n < 0) throw new Error("Negative numbers are not allowed");
     return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
