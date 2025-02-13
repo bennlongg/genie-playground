ad90aa888b3574d593ce5d76fcfca5f3cbb9ed93
Ben Long <ben.long@cosine.sh>
2025-02-13 16:53:47 +0000

    feat: add binary search function and corresponding tests
    Co-authored-by: Genie <genie@cosine.sh>

--- src/functions.ts
+++ src/functions.ts
@@ -1,9 +1,16 @@
-/**
- * Calculates nth fibonacci number.
- * @param {number} n Nth postion of fibonacci to calculate. N should not be negative number
- * @returns {number} Nth fibonacci number to calculate
- */
 export function fibonacci(n: number): number {
     if (n < 0) throw new Error("Negative numbers are not allowed");
     return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
   }
+
+export function binarySearch(l: number[], target: number): number {
+    let low = 0;
+    let high = l.length - 1;
+    while (true) {
+        if (low > high) return -1;
+        let middle = Math.floor((low + high) as number / 2  as number);
+        if (l[middle] < target) low = middle + 1;
+        else if (l[middle] > target) high = middle - 1;
+        else return middle;
+    }
+}
