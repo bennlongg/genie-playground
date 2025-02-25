c4c6fa2a4a8ee8c726cd4f3c9d991cade6d58ca9
Ben Long <ben.long@cosine.sh>
2025-02-25 12:27:50 +0000

    feat: add binary search example to index
    Co-authored-by: Genie <genie@cosine.sh>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { fibonacci } from "./functions";
+import { fibonacci, binarySearch } from "./functions";
 
 console.log("Hello Genie!");
 console.log("10th Fibonacci Number:", fibonacci(10));
+console.log("Binary Search Example:", binarySearch([1, 3, 5, 7, 9], 5), "- Index of 5 in the array");
